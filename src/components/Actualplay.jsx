acef42283197c457d5bd8e1d4104fbd9cd745d4a
Vidhi Khaitan <vidhik2002@gmail.com>
2021-03-13 19:54:52 +0530

    feat: added word generator

--- src/components/Actualplay.jsx
+++ src/components/Actualplay.jsx
@@ -2,18 +2,48 @@ import React, { Component } from 'react'
 import Game from './Game'
 import './buttons.css'
 import firebase from '../firebase'
+import Sound1 from './sounds/poping-up.mp3'
+import {Howl,Howler} from "howler";
+
+
+
+
+// const audioClips = [
+//     {sound:Sound1, label:'Start'},
+//     ]
+//     SoundPlay = (src) => {
+//         const sound = new Howl({
+//             src
+//         })
+//         sound.play();
+//     }
+//     RenderButtonAndSound = () => {
+//         return audioClips.map((soundObj, index) => {
+//             <button key={index} onClick={() => this.SoundPlay(soundObj.sound)}>
+//                 {soundObj.label}
+//             </button>
+//         })
+//     }
 
 export default class Actualplay extends Component {
     state={
         visible:true
     };
+    
+    
     render() {
+    Howler.volume(100.0)
     return (
         <div>
             {this.state.visible ? (
             <div className="afterlogin">
                 <ul>
-                <li><button className="buttons" onClick={()=>{this.setState({visible:false});}}>Start</button> </li>
+                <li><button className="buttons" 
+                 onClick={()=>{
+                this.setState({visible:false});
+            }}>
+                Start
+                </button> </li>
                 <br></br>
                 <li><button className="buttons" onClick={()=>firebase.auth().signOut()}>Sign out</button> </li>
                 </ul>
@@ -22,7 +52,7 @@ export default class Actualplay extends Component {
             :
             (<div>
             <Game/>
-            <button onClick={()=>firebase.auth().signOut()}> Sign out</button>
+            <button className="buttons" onClick={()=>firebase.auth().signOut()}> Sign out</button>
             </div>)
             }
         </div>
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,20 +1,42 @@
-import React from 'react'
-import './Game.css' 
+import React, { useState, Component } from 'react'
+import useKeyPress from '../hooks/useKeyPress';
+import { generate } from '../utils/words';
+import './Game.css'
+import wordarray from "./file.json"
 
-const Checking=<h1>Hello </h1>
 
 
+var myarray = wordarray.wordlist;
+
+var randomItem1 = myarray[Math.floor(Math.random()*myarray.length)];
+var randomItem2 = myarray[Math.floor(Math.random()*myarray.length)];
+var randomItem3 = myarray[Math.floor(Math.random()*myarray.length)];
+var randomItem4 = myarray[Math.floor(Math.random()*myarray.length)];
+var randomItem5 = myarray[Math.floor(Math.random()*myarray.length)];
 
 function Game() {
+    useKeyPress(key => {
+    console.log(key)
+    console.log("hi");
+    
+  });
+  
     return (
         <div className='Menu'>
             <div className="videodiv">
                 <div className="textwords"> 
-                {Checking}
+                <h1>{randomItem1}</h1>
+                <h1>{randomItem2}</h1>
+                <h1>{randomItem3}</h1>
+                <h1>{randomItem4}</h1>
+                <h1>{randomItem4}</h1>
+
                 </div>
+                
                 <video className='gamebg' src='/videos/gamebg.mp4' autoPlay muted loop></video>
             </div>
         </div>
+        
     )
 }
 
